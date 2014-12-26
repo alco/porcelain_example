820c8c498ec37b9d80cf90fa39589e6de58e9761
Alexei Sholik <alcosholik@gmail.com>
2014-12-27 01:38:00 +0200

    Update Elixir version requirement to support 1.x

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PorcelainExample.Mixfile do
   def project do
     [app: :porcelain_example,
      version: "0.1.0",
-     elixir: "~> 0.14",
+     elixir: ">= 0.14.0 and < 2.0.0",
      deps: deps]
   end
 
